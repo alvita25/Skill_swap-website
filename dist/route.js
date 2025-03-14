ad384befe52c11210e8bc77097f9138701209619
alvita25 <alvitamd25@gmail.com>
2025-03-14 14:37:36 +0530

    updated toml file

--- dist/route.js
+++ dist/route.js
@@ -15,9 +15,9 @@ const prerequisiteRoute = require('./src/routes/prerequisites.js');
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // use routes
 app.use('/signup', signupRoute);
